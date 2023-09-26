c4068ec6bee81b263c2b58dcb9d8ad7796bc4bc7
Sam Whyte <142351152+swhyte99@users.noreply.github.com>
2023-09-25 19:53:27 -0500

    Add files via upload

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,10 +11,13 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = ["Who was the first American woman in space? " ,
+ "True or false: 5 kilometer == 5000 meters? "  ,
+ "(5 + 3)/2 * 10 = ? " ,
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " ,
+  "What is the minimum crew size for the ISS? " ];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers;
-
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -27,17 +30,21 @@ candidateName = input.question("What's your name? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let candidateAnswer = input.question(question);
-
+let candidateAnswers = []
+for (let i = 0; i < questions.length; i++){
+  let answer = input.question(questions[i]);
+  candidateAnswers.push(answer);
+}
 }
 
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz() {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log('Correct!');
-} else {
-  console.log("Incorrect!");
+for (let i = 0; i < questions.length; i++){
+  console.log(`Question ${i + 1}: ${questions[i]}`);
+  console.log(`Candidate's Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}`);
+  console.log("/n");
 }
 
 
